af7882331b8bc7c31d3ccc40e2aff48b9c533cbb
bernardogomes <bgomesdeabreu@gmail.com>
2015-04-10 09:09:54 -0300

    Commit adicionado botao reiniciar.

--- static/js/jogoDaVelhaController.js
+++ static/js/jogoDaVelhaController.js
@@ -6,24 +6,25 @@ app_jogo_da_velha.controller('JogoDaVelhaCtrl', function ($scope) {
     $scope.jogadaDoPrimeiro = true;
     $scope.JogadorVencedor = 0;
     $scope.JogadorVenceu = false;
+    $scope.DeuVelha = false;
+    $scope.jogo;
 
-
-    $scope.iniciarJogo = function(linha, coluna){
+    $scope.iniciarJogo = function(){
         $scope.jogadaDoPrimeiro = true;
         $scope.JogadorVencedor = 0;
         $scope.JogadorVenceu = false;
-
-        var matriz = [];
-        for (var i = 0; i < linha; i++) {
-            matriz[i] = [];
-            for (var j = 0; j < coluna; j++) {
-                matriz[i][j] = 0;
+        $scope.DeuVelha = false;
+        $scope.jogo = [];
+        for (var i = 0; i < $scope.linhas; i++) {
+            $scope.jogo[i] = [];
+            for (var j = 0; j < $scope.colunas; j++) {
+                $scope.jogo[i][j] = '';
             };
         };
-        return matriz;
     };
 
-    $scope.jogo = $scope.iniciarJogo($scope.linhas, $scope.colunas);
+    $scope.iniciarJogo();
+    
     $scope.getArray = function(num) {
         var _Array = [];
         for (var i = 0; i < num; i++) 
@@ -33,71 +34,75 @@ app_jogo_da_velha.controller('JogoDaVelhaCtrl', function ($scope) {
 
     $scope.jogada = function(lin,col){
         
-        if($scope.jogo[lin][col] === 0){
+        if($scope.jogo[lin][col] === ''){
             $scope.jogadaDoPrimeiro = !$scope.jogadaDoPrimeiro;
-            if (!$scope.jogadaDoPrimeiro) {
-                $scope.jogo[lin][col] = 1
-                $scope.verificarSeExisteGanhador();
-                $scope.MostrarVencedor($scope.JogadorVencedor);
-                return "bolinha"
-            }
-            else {
-                $scope.jogo[lin][col] = 2;
-                $scope.verificarSeExisteGanhador();
-                $scope.MostrarVencedor($scope.JogadorVencedor);
-                return "xis";
-            }
-        }else{
-            if ($scope.jogo[lin][col] ===1) return "bolinha"
-            else return "xis";
+            if (!$scope.jogadaDoPrimeiro) 
+                $scope.jogo[lin][col] = "bolinha";
+            else 
+                $scope.jogo[lin][col] = "xis";
+            $scope.verificarSeExisteGanhador();
+            $scope.MostrarVencedor($scope.JogadorVencedor);
+            if ($scope.JogadorVencedor === 0) $scope.verificaSeDeuVelha();
         }
     };
 
     $scope.verificarSeExisteGanhador = function(){
-        var Jogador = 1;
+        var Jogador = "bolinha";
         verifica: {
             for (var i = 0; i < 2; i++) {
-                if($scope.jogo[0][i] === Jogador
-                    && $scope.jogo[1][i] === Jogador
-                    && $scope.jogo[2][i] === Jogador ){
-                        $scope.JogadorVencedor = Jogador;
-                        break verifica;
-                    }
+                for (var j = 0; j < 3; j++) {
+                    if($scope.jogo[0][j] === Jogador
+                        && $scope.jogo[1][j] === Jogador
+                        && $scope.jogo[2][j] === Jogador ){
+                            $scope.JogadorVencedor = Jogador;
+                            break verifica;
+                        }
 
-                else if($scope.jogo[i][0] === Jogador
-                    && $scope.jogo[i][1] === Jogador
-                    && $scope.jogo[i][2] === Jogador ){
-                        $scope.JogadorVencedor = Jogador;
-                        break verifica;
-                    }
+                    else if($scope.jogo[j][0] === Jogador
+                        && $scope.jogo[j][1] === Jogador
+                        && $scope.jogo[j][2] === Jogador ){
+                            $scope.JogadorVencedor = Jogador;
+                            break verifica;
+                        }
 
-                else if($scope.jogo[0][0] === Jogador
-                    && $scope.jogo[1][1] === Jogador
-                    && $scope.jogo[2][2] === Jogador ){
-                        $scope.JogadorVencedor = Jogador;
-                        break verifica;
-                    }
-                else if($scope.jogo[0][2] === Jogador
-                    && $scope.jogo[1][1] === Jogador
-                    && $scope.jogo[2][0] === Jogador ){
-                        $scope.JogadorVencedor = Jogador;
-                        break verifica;
-                    }
-                Jogador = 2;
+                    else if($scope.jogo[0][0] === Jogador
+                        && $scope.jogo[1][1] === Jogador
+                        && $scope.jogo[2][2] === Jogador ){
+                            $scope.JogadorVencedor = Jogador;
+                            break verifica;
+                        }
+                    else if($scope.jogo[0][2] === Jogador
+                        && $scope.jogo[1][1] === Jogador
+                        && $scope.jogo[2][0] === Jogador ){
+                            $scope.JogadorVencedor = Jogador;
+                            break verifica;
+                        }
+                }
+                Jogador = "xis";
             };
         }
     }
+
+    $scope.verificaSeDeuVelha = function(){
+        var deuVelha = true;
+        for (var i = 0; i < $scope.linhas; i++) {
+            for (var j = 0; j <  $scope.colunas; j++) {
+                if($scope.jogo[i][j] === '') deuVelha = false;
+            };
+        }; 
+        if(deuVelha) $scope.DeuVelha = deuVelha
+
+    };
+
     $scope.MostrarVencedor = function(Vencedor){
-        if(Vencedor===1 || Vencedor===2){
+        if (Vencedor !== 0){
+            if(Vencedor === "bolinha"){
+                $scope.JogadorVencedor = 1
+            }
+            else if(Vencedor === "xis"){
+                $scope.JogadorVencedor = 2
+            }
             $scope.JogadorVenceu = true;
         }
-    }
-
-    $scope.LimparClass = function(){
-        for (var i = 0; i < $scope.linhas; i++) {
-            for (var j = 0; i < $scope.colunas; i++) {
-
-            };
-        };
     }
 });
